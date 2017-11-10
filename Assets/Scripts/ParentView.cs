1908c45a4b0c0bb51a4bdfbc395ee16dbcf317ee
FNGNATOR\lopez <lopezmramon@gmail.com>
2017-11-10 14:03:04 -0300

    After a lot of searching, defined MainMenuView should be a member of at least 1 window page group, so that it can transition to it. Now it's a member of the Window Page Group that's in Launcher view. Created a prefab for the profile dropdown that looks much better. Will add functionality this afternoon.

---
+++ Assets/Scripts/ParentView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Devdog.General.UI;
+
+public class ParentView : MonoBehaviour
+{
+
+    List<UIWindowPage> allPages = new List<UIWindowPage>();
+    // Use this for initialization
+    void Start()
+    {
+        foreach (UIWindowPage uiWindow in GetComponentsInChildren<UIWindowPage>())
+        {
+            allPages.Add(uiWindow);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DeactivateAllPagesButOne(UIWindowPage stayActive)
+    {
+        foreach(UIWindowPage page in allPages)
+        {
+            if(page != stayActive)
+            {
+                page.Hide();
+            }
+        }
+    }
+}
