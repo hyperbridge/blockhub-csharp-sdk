67f773f482c2cafdde4b132a52d8aea8c5655762
Eric Muyser <eric@muyser.com>
2017-12-07 15:16:37 +0900

    Clean up

--- Assets/DoozyUI/FontAwesome/Examples/Scripts/FontAwesomeEditorExample.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace DoozyUI.FontAwesome
-{
-    public class FontAwesomeEditorExample : MonoBehaviour
-    {
-        //We use this class just to show you the editor code. Look inside the Editor folder for it's custom Editor.
-    }
-}
--- Assets/Standard Assets/Loading Effect/Content/Scripts/Internal/Enum/LoadingType.cs
+++
@@ -1,5 +0,0 @@
-﻿public enum LoadingEffectType
-{
-    Rotate,
-    Filled,
-}
