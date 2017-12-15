562d18e7768bd3ea3e235e88287d7ceaada736cd
FNGNATOR\lopez <lopezmramon@gmail.com>
2017-12-15 15:29:56 -0300

    Notification improvements.

--- Scripts/Views/MainView.cs
+++ Scripts/Views/MainView.cs
@@ -8,59 +8,7 @@ namespace Hyperbridge.UI
     public class MainView : MonoBehaviour
     {
 
-        public Text notificationAmountText;
-        ProfileData activeProfile;
-
-        void Awake()
-        {
-            CodeControl.Message.AddListener<EditProfileEvent>(OnProfileEdited);
-            CodeControl.Message.AddListener<ProfileInitializedEvent>(OnProfileInitialized);
-        }
-
-        void OnProfileEdited(EditProfileEvent e)
-        {
-            if (e == null)
-            {
-                throw new System.ArgumentNullException(nameof(e));
-            }
-
-            if (activeProfile == null) return;
-            if (activeProfile.notifications == null)
-            {
-                notificationAmountText.text = "0";
-
-                return;
-
-            }
-
-            UpdateNotificationsText(activeProfile.notifications.Count.ToString());
-        }
-
-        void OnProfileInitialized(ProfileInitializedEvent e)
-        {
-
-            if (e == null)
-            {
-                throw new System.ArgumentNullException(nameof(e));
-            }
-
-            if (e.activeProfile == null) return;
-            if (e.activeProfile.notifications == null)
-            {
-                notificationAmountText.text = "0";
-
-                return;
-
-            }
-            UpdateNotificationsText(e.activeProfile.notifications.Count.ToString());
-            activeProfile = e.activeProfile;
-        }
-
-        void UpdateNotificationsText(string amount)
-        {
-            notificationAmountText.text = amount;
-
-        }
+     
     }
 }
 
