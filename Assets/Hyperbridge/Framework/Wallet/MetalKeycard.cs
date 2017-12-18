c282f918c9852483d47c47fc7b62aa08a0e258d8
FNGNATOR\lopez <lopezmramon@gmail.com>
2017-12-18 16:44:51 -0300

    Added keycard prefab and script. Adjusted some UI.

---
+++ Assets/Hyperbridge/Framework/Wallet/MetalKeycard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Hyperbridge.Wallet
+{
+    public class MetalKeycard : MonoBehaviour
+    {
+
+        public Image QRCode;
+        public Text jsonText, accountNameText, dateText, userKeyText;
+
+        public void SetupMetalKeycard (string jsonText, string accountName, string key)
+        {
+            this.jsonText.text = jsonText;
+            this.accountNameText.text = accountName;
+            this.userKeyText.text = key;
+            var currentDateTime = System.DateTime.Now;
+            dateText.text = "Created on " + currentDateTime.Day + "," + currentDateTime.Month + " " + currentDateTime.Day + " " + currentDateTime.Year + " for account";
+        }
+    }
+}
+
